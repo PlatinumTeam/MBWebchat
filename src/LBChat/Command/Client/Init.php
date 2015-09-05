70fbeff7832c01b3fc6ec6ef12a9a987a5d162cf
HiGuy Smith <higuymb@gmail.com>
2015-09-05 03:19:17 -0400

    Really sad class loading

--- src/LBChat/Command/Client/Init.php
+++ src/LBChat/Command/Client/Init.php
@@ -1,1 +1,37 @@
 <?php
+namespace LBChat\Command\Client;
+
+use LBChat\ChatClient;
+use LBChat\ChatServer;
+use LBChat\Command\CommandFactory;
+
+function _addCommands($array) {
+	foreach ($array as $class) {
+		if (is_array($class)) {
+			_addCommand($class[0], $class[1]);
+		} else {
+			_addCommand($class);
+		}
+	}
+}
+
+function _addCommand($name, $class = null) {
+	if ($class === null)
+		$class = "LBChat\\Command\\Client\\{$name}Command";
+
+	CommandFactory::addCommandType($name, function(ChatClient $client, ChatServer $server, $rest) use ($class) {
+		return call_user_func(array($class, "init"), $client, $server, $rest);
+	});
+}
+
+//Add all the commands
+_addCommands(
+	array(
+		"Chat",
+		"Identify",
+		"Key",
+		"Location",
+		"Ping",
+		"Userlist"
+	)
+);
--- src/LBChat/Command/CommandFactory.php
+++ src/LBChat/Command/CommandFactory.php
@@ -4,8 +4,10 @@ namespace LBChat\Command;
 use LBChat\ChatClient;
 use LBChat\ChatServer;
 use LBChat\Command\Client\IClientCommand;
+use LBChat\Command\Client\IdentifyCommand;
 
 abstract class CommandFactory {
+	static $commandTypes = array();
 	/**
 	 * Construct a client command object from a given message, that can be executed.
 	 * @return IClientCommand
@@ -18,15 +20,19 @@ abstract class CommandFactory {
 		$first = array_shift($words);
 		$rest = implode(" ", $words);
 
-		switch ($first) {
-		case "CHAT":     return new Client\ChatCommand    ($client, $server, $rest);
-		case "IDENTIFY": return new Client\IdentifyCommand($client, $server, $rest);
-		case "KEY":      return new Client\KeyCommand     ($client, $server, $rest);
-		case "LOCATION": return new Client\LocationCommand($client, $server, $rest);
-		case "PING":     return new Client\PingCommand    ($client, $server, $rest);
-		case "USERLIST": return new Client\UserlistCommand($client, $server       );
+		if (array_key_exists($first, self::$commandTypes)) {
+			$constructor = self::$commandTypes[$first];
+			return call_user_func($constructor, $client, $server, $rest);
+		} else {
+			return null;
 		}
+	}
 
-		return null;
+	public static function addCommandType($name, $class) {
+		self::$commandTypes[strtoupper($name)] = $class;
+	}
+
+	public static function init() {
+		require "Client/Init.php";
 	}
 }
