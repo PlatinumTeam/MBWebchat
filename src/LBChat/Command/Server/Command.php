c4c68241203cc070313e401ca3aa5749b0cde6b8
HiGuy Smith <higuymb@gmail.com>
2015-09-05 02:18:45 -0400

    Extra params

--- src/LBChat/Command/Server/Command.php
+++ src/LBChat/Command/Server/Command.php
@@ -8,14 +8,6 @@ abstract class Command {
 	 * @var ChatServer $server The chat server
 	 */
 	protected $server;
-	/**
-	 * @var string $name The command's name
-	 */
-	protected $name;
-	/**
-	 * @var string $data The command message data
-	 */
-	protected $data;
 
 	public function __construct(ChatServer $server) {
 		$this->server = $server;
