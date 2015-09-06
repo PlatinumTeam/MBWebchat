3c709bdaab557a4a17a39b3ec333eee188fba04e
HiGuy Smith <higuymb@gmail.com>
2015-09-05 23:24:28 -0400

    don't crash here

--- src/LBChat/Command/Chat/WhisperCommand.php
+++ src/LBChat/Command/Chat/WhisperCommand.php
@@ -9,24 +9,26 @@ use LBChat\Utils\String;
 
 class WhisperCommand extends Command implements IChatCommand {
 
-    protected $recipient;
+	protected $recipient;
 
-    protected $message;
+	protected $message;
 
-    public function __construct(ChatClient $client, ChatServer $server, ChatClient $recipient, $message) {
-        parent::__construct($client, $server);
-        $this->recipient = $recipient;
-        $this->message = $message;
-    }
+	public function __construct(ChatClient $client, ChatServer $server, ChatClient $recipient = null, $message) {
+		parent::__construct($client, $server);
+		$this->recipient = $recipient;
+		$this->message = $message;
+	}
 
-    public function execute() {
-        $chat = new ChatCommand($this->server, $this->client, $this->recipient, $this->message);
-        $chat->execute($this->recipient);
-    }
+	public function execute() {
+		$chat = new ChatCommand($this->server, $this->client, $this->recipient, $this->message);
+		$chat->execute($this->recipient);
+	}
 
-    public static function init(ChatClient $client, ChatServer $server, $rest) {
-        $words = explode(" ", $rest);
-        $recipient = $server->findClient(String::decodeSpaces(array_shift($words)));
-        return new WhisperCommand($client, $server, $recipient, "/whisper " . $rest);
-    }
+	public static function init(ChatClient $client, ChatServer $server, $rest) {
+		$words = explode(" ", $rest);
+		$recipient = $server->findClient(String::decodeSpaces(array_shift($words)));
+		if ($recipient === null)
+			return null;
+		return new WhisperCommand($client, $server, $recipient, "/whisper " . $rest);
+	}
 }
