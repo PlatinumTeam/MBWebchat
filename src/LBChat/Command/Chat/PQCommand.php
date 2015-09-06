5c75ec329aab0ad6061fc3eb0b4b7d404be8c45a
HiGuy Smith <higuymb@gmail.com>
2015-09-06 18:59:29 -0400

    PQ WHERe?

--- src/LBChat/Command/Chat/PQCommand.php
+++ src/LBChat/Command/Chat/PQCommand.php
@@ -4,10 +4,11 @@ namespace LBChat\Command\Chat;
 use LBChat\ChatClient;
 use LBChat\ChatServer;
 use LBChat\Command\Server\ChatCommand;
+use LBChat\Misc\ServerChatClient;
 
 class PQCommand extends Command implements IChatCommand {
 	public function execute() {
-		$chat = new ChatCommand($this->server, $this->client, null, "PQ!");
+		$chat = new ChatCommand($this->server, ServerChatClient::getClient(), null, "PQ WHERe?");
 		$this->server->broadcastCommand($chat);
 	}
 
