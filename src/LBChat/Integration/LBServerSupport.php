992cc9bda5a558c3411c8d6604a0027571652bbc
HiGuy Smith <higuymb@gmail.com>
2015-11-14 13:33:50 -0500

    QotD

--- src/LBChat/Integration/LBServerSupport.php
+++ src/LBChat/Integration/LBServerSupport.php
@@ -37,8 +37,34 @@ abstract class LBServerSupport {
 	}
 
 	public static function getWelcomeMessage($webchat = false) {
-		if ($webchat)
-			return self::getPreference("webwelcome");
-		return self::getPreference("welcome") . "\\n\\nTODO: Qotd";
+		$message = self::getPreference($webchat ? "webwelcome" : "welcome");
+
+		//Get qotd from the database
+		$query = self::$database->prepare("SELECT * FROM `qotd` WHERE `selected` = 1");
+		$query->execute();
+
+		//We may have more than one, pluralize the name if we do
+		if ($query->rowCount() == 1) {
+			$message .= "\n\nLeaderboards' Quote of the Day: ";
+		} else if ($query->rowCount() > 1) {
+			$message.= "\n\nLeaderboards' Quotes of the Day:";
+		}
+
+		$rows = $query->fetchAll(\PDO::FETCH_ASSOC);
+		foreach ($rows as $row) {
+			$text = $row["text"];
+			$user = $row["username"];
+			$time = $row["timestamp"];
+
+			//Get current year
+			$year = (new \DateTime($time))->format("Y");
+
+			//"quote" -Name Year
+			$message .= "\n\"$text\" -$user $year";
+		}
+		//Escape newlines so we don't send this all as multiple messages
+		$message = str_replace("\n", "\\n", $message);
+
+		return $message;
 	}
 }
