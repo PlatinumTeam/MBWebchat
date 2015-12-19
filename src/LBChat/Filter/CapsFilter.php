51a4b2b8fc19e29786340b45a8c5f991ed386711
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-12-19 15:54:40 -0500

    new caps filter

--- src/LBChat/Filter/CapsFilter.php
+++ src/LBChat/Filter/CapsFilter.php
@@ -16,22 +16,12 @@ class CapsFilter extends ChatFilter {
 		// first things first. strip formatting
 		$filteredMessage = String::stripFormatting($message);
 
-		$len = strlen($filteredMessage);
-
-		// check to see if everything is caps
-		preg_match("/^[A-Z0-9_]+/", $filteredMessage, $matches);
-		if (count($matches) == $len) {
-			// every character was caps, numbers, or _
-			return false;
-		}
-
 		// check to see if at least 50% of the message was caps.
-		// if it was, reject the message.
-		preg_match("/[A-Z]*/", $filteredMessage, $matches);
-		$half = (int)floor($len / 2);
-		if (count($matches) >= $half) {
-			// at least half were caps lock'd, fail
-			return false;
+		// if it was, replace the entire message with lowercase letters!
+		$half = (int)floor(strlen($filteredMessage) / 2);
+		if (preg_match_all("/(?<!\\w)\\w/", $filteredMessage, $matches) >= $half) {
+			// lowercase the actual message
+			preg_replace("/(?<!\\w)\\w/", "?<=\\w)\\w", $message);
 		}
 
 		return true;
